436362aa8607291ef36c604ce2488581e43feeb1
Anks <dubeyankur2833@gmail.com>
2024-03-31 21:47:39 +0530

    sixth commit

--- app/Components/Reviews.jsx
+++ app/Components/Reviews.jsx
@@ -2,24 +2,26 @@
 
 import React, { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { FaTripadvisor } from "react-icons/fa";
 
 import { Pagination } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
 import Review from './Review';
+import Link from 'next/link';
 
 const Reviews = () => {
 
     const reviewData = [
-        {id: 1, name: "Vidhi", date: "December 19, 2022.", para: "Wonderful wonderful wonderful I stayed with Outback for many nights, I kept extending and lost track eventually. The hospitality is...", image: "/asset4.jpeg"},
-        {id: 2, name: "Pragati Bansal", date: "October 6, 2022.", para: "The Island HOME I had travelled to Andamans with friends for a much-needed break from the hectic work life, that is when we stayed at...", image: "/asset5.jpeg"},
-        {id: 3, name: "Pooja N", date: "September 30, 2022.", para: "Comfortable, fun, best beach Best place to stay for solo travellers. You will meet soon many like minded people and indulge in fun...", image: "/asset6.jpeg"},
-        {id: 4, name: "Digant Bhatt", date: "November 17, 2021.", para: "Eco Friendly - Neat and Clean Resort Very nice place to spend time. Rooms and Bathrooms are...", image: "/asset9.jpeg"},
-        {id: 5, name: "Ranjeeta s", date: "October 23, 2021.", para: "Homely, Friendly, Excellent So greenery, friendly staff specially Rana ji and Commando.. all were so loving and helping. We stayed her...", image: "/asset10.jpeg"},
-        {id: 6, name: "Shivon D", date: "May 10, 2021.", para: "Great location, staff and vibe Outback is located bang opposite one of the most serene beaches, its a 1 minute walk across the road...", image: "/asset11.jpeg"},
-        {id: 7, name: "Shramona Poddar", date: "April 27, 2021.", para: "Wonderful stay in the heart of Havelock My eyes first spotted the wall in the room adorned with bottles that...", image: "/asset12.jpeg"},
-        {id: 8, name: "Manik Thakar", date: "April 3, 2021.", para: "Amazing experience! We went as a group of 12 students on our college grad trip. We had the best of times at the place. A...", image: "/asset13.jpeg"},
+        { id: 1, name: "Vidhi", date: "December 19, 2022.", para: "Wonderful wonderful wonderful I stayed with Outback for many nights, I kept extending and lost track eventually. The hospitality is...", image: "/asset4.jpeg" },
+        { id: 2, name: "Pragati Bansal", date: "October 6, 2022.", para: "The Island HOME I had travelled to Andamans with friends for a much-needed break from the hectic work life, that is when we stayed at...", image: "/asset5.jpeg" },
+        { id: 3, name: "Pooja N", date: "September 30, 2022.", para: "Comfortable, fun, best beach Best place to stay for solo travellers. You will meet soon many like minded people and indulge in fun...", image: "/asset6.jpeg" },
+        { id: 4, name: "Digant Bhatt", date: "November 17, 2021.", para: "Eco Friendly - Neat and Clean Resort Very nice place to spend time. Rooms and Bathrooms are...", image: "/asset9.jpeg" },
+        { id: 5, name: "Ranjeeta s", date: "October 23, 2021.", para: "Homely, Friendly, Excellent So greenery, friendly staff specially Rana ji and Commando.. all were so loving and helping. We stayed her...", image: "/asset10.jpeg" },
+        { id: 6, name: "Shivon D", date: "May 10, 2021.", para: "Great location, staff and vibe Outback is located bang opposite one of the most serene beaches, its a 1 minute walk across the road...", image: "/asset11.jpeg" },
+        { id: 7, name: "Shramona Poddar", date: "April 27, 2021.", para: "Wonderful stay in the heart of Havelock My eyes first spotted the wall in the room adorned with bottles that...", image: "/asset12.jpeg" },
+        { id: 8, name: "Manik Thakar", date: "April 3, 2021.", para: "Amazing experience! We went as a group of 12 students on our college grad trip. We had the best of times at the place. A...", image: "/asset13.jpeg" },
     ]
 
     return (
@@ -42,9 +44,19 @@ const Reviews = () => {
 
                 <h1 className='text-[2vw] sm:text-[1vw]'>Based on <strong>69 reviews</strong></h1>
 
-                <div className='flex items-center gap-1'>
-                    <img className='w-[4vw] sm:w-[2vw]' src="/asset28.svg" alt="noimg" />
-                    <h1>Tripadvisor</h1>
+                <div className='flex items-center gap-1 transition-all'>
+
+                    <Link
+                        href={"https://www.tripadvisor.in/Hotel_Review-g503691-d14079532-Reviews-Outback_Resorts-Havelock_Island_Andaman_and_Nicobar_Islands.html"}
+                        target='_blank'
+                        className="text-[#34E0A1] hover:text-black bg-white hover:bg-[#34E0A1] rounded-full border-2 border-[#34E0A1] px-1 py-1 transition-all"
+                    >
+                        <FaTripadvisor />
+                    </Link>
+
+                    <Link href={"https://www.tripadvisor.in/Hotel_Review-g503691-d14079532-Reviews-Outback_Resorts-Havelock_Island_Andaman_and_Nicobar_Islands.html"} target='_blank'>
+                        <h1 className=''>Tripadvisor</h1>
+                    </Link>
                 </div>
             </div>
 
@@ -61,8 +73,8 @@ const Reviews = () => {
                     className="mySwiper"
                 >
 
-                    {reviewData.map((rev, index)=>{
-                       return <SwiperSlide><Review key={rev.id} name={rev.name} date={rev.date} para={rev.para} image={rev.image}  /></SwiperSlide> 
+                    {reviewData.map((rev, index) => {
+                        return <SwiperSlide><Review key={rev.id} name={rev.name} date={rev.date} para={rev.para} image={rev.image} /></SwiperSlide>
                     })}
 
                 </Swiper>
